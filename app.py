d336bf64f861db9e6e26b006472c2c87bed2abf6
Hoàng Trọng Bình <62093518+hoangtrongbinh1111@users.noreply.github.com>
2021-01-31 15:18:50 +0700

    Binh commit

--- app.py
+++ app.py
@@ -1,55 +1,132 @@
-from flask import Flask,render_template,request,json
+from flask import Flask,render_template,request,json,redirect,url_for
 import sqlite3
 app = Flask(__name__)
 
+@app.route("/login",methods = ['POST', 'GET'])
+def login():
+    msg=""
+    if request.method=='POST':
+        username=request.form['username']
+        with sqlite3.connect("database.db") as conn:
+            try:                    
+                cur=conn.cursor()
+                cur.execute("INSERT INTO user (username)  values ('{0}')".format(username))
+                conn.commit()                               
+            except:
+                conn.rollback()
+                # msg="Username is existed!"    
+                # return render_template('login.html',msg=msg)             
+            # finally:
+            #     conn.close()  
+        return redirect(url_for('labelPlate',username=username))   
+    return render_template('login.html',msg=msg)
 
-@app.route("/")
-@app.route("/home")
+@app.route("/",methods = ['POST', 'GET'])
+@app.route("/home",methods = ['POST', 'GET'])
 def home():
-    return render_template('login.html')
+    msg=""
+    # if request.method=='POST':
+    #     username=request.form['username']
+    #     with sqlite3.connect("database.db") as conn:
+    #         try:                    
+    #             cur=conn.cursor()
+    #             cur.execute("INSERT INTO user (username) values (?)",(username))
+    #             conn.commit()  
+    #             return render_template('labelPlate.html',username=username)                 
+    #         except:
+    #             conn.rollback()
+    #             msg="Username is existed!"    
+    #             return render_template('login.html',msg=msg)             
+    #         # finally:
+    #         #     conn.close()  
+    # return render_template('login.html',msg=msg)
+    return render_template('login.html',msg=msg)
 
-@app.route("/upload")
+
+@app.route("/upload",methods = ['POST', 'GET'])
 def upload():
+    if request.method=='POST':
+        filename=request.form['filename']
+        lst=filename.split("**")
+        conn=sqlite3.connect("database.db")
+        cur=conn.cursor()
+        error=""
+        for item in lst:
+            if item=="":
+                continue
+            try:
+                cur.execute("INSERT INTO dataset_img (filename) values ('{0}')".format(item))
+            except:
+                error=error+item+"**"
+                continue
+        conn.commit()
+        msg="Upload to database success!"
+        return json.dumps({'msg':msg,'error':error})
     return render_template('upload.html')
 
 
-@app.route('/labelPlate',methods = ['POST', 'GET'])
-def labelPlate():
-    return render_template('labelPlate.html')
+@app.route('/labelPlate/<username>')
+def labelPlate(username):
+    conn=sqlite3.connect("database.db")
+    conn.row_factory=sqlite3.Row
+    cur=conn.cursor()
+    #get random 5 images
+    cur.execute("select * from dataset_img where status=0 order by random() limit 5")
+    rows=cur.fetchall()
+    #get number of images user labeled
+    cur.execute("select * from dataset_img where username='{0}'".format(username))
+    count=len(cur.fetchall())
+    #get remains
+    cur.execute("select * from dataset_img where status=0")
+    remains=len(cur.fetchall())
+    return render_template('labelPlate.html',username=username,rows=rows,count=count,remains=remains)
 
 @app.route('/sendPlt',methods = ['POST', 'GET'])
 def sendPlt():
     if request.method=='POST':
-        lb=request.form['label']
-        user=request.form['user']
-        img_name=request.form['img_name']
+        lb=request.form['listID_Label'].split("/")
+        user=request.form['user']        
+        data=dict()
+        for item in lb:
+            if item=="":
+                continue
+            tmp=item.split(":")
+            data.update({tmp[0]:tmp[1]})
+        error=""
         with sqlite3.connect("database.db") as conn:
-            try:
-                flag=checkExistInDB_Plate(user,img_name)
-                if flag==True:
-                    msg="You had label this plate"
-                    return json.dumps({'msg':msg})
-                cur=conn.cursor()
-                cur.execute("INSERT INTO Plate (username,image_name,labelPlate) values (?,?,?)",(user,img_name,lb))
-                conn.commit()
-                msg="Add successful"
-            except:
-                conn.rollback()
-                msg="can't be add"  
-            finally:
-                return json.dumps({'msg':msg})
-                conn.close()              
+            cur=conn.cursor()
+            for key in data.keys():
+                try:                               
+                    cur.execute("Update dataset_img set label='{0}',status=1,username='{1}' where id={2} and status=0 ".format(data[key],user,key))                             
+                except:
+                    error=error+"Wrong in id="+key+"\r\n"
+                    continue
+            conn.commit()          
+        return json.dumps({'error':error})             
     return "Failed"
 
-@app.route('/viewPlt')
-def viewPlt():
+@app.route('/delete_table_img',methods=['POST'])
+def delete_table_img():
+    if request.method=='POST':
+        conn=sqlite3.connect("database.db")
+        error=" "
+        try:
+            cur=conn.cursor()
+            cur.execute("delete from dataset_img")
+            conn.commit()
+        except:
+            error="Không thể xóa dataset"
+        return json.dumps({'error':error})
+    return "OK"
+@app.route('/viewImg/<username>')
+def viewImg(username):
     conn=sqlite3.connect("database.db")
     conn.row_factory=sqlite3.Row
     cur=conn.cursor()
-    cur.execute("select * from Plate")
+    cur.execute("select * from dataset_img where username='{0}'".format(username))
     #cur.execute("select * from Plate where username='Binh'")
     rows=cur.fetchall()
-    return render_template("viewPlt.html",rows=rows)
+    return render_template("viewImg.html",rows=rows)
 
 def checkExistInDB_Plate(user,img_name):
     conn=sqlite3.connect("database.db")
@@ -60,5 +137,6 @@ def checkExistInDB_Plate(user,img_name):
     if not rows:
         return False
     return True
+
 if __name__ == '__main__':
     app.run(debug=True)
--- db.py
+++ db.py
@@ -3,7 +3,10 @@ from flask import current_app,g
 conn = sqlite3.connect('database.db')
 print ("Opened database successfully")
 
-# conn.execute('CREATE TABLE students (name TEXT, addr TEXT, city TEXT, pin TEXT)')
-conn.execute('CREATE TABLE Plate (id INTEGER PRIMARY KEY AUTOINCREMENT,username TEXT UNIQUE NOT NULL,image_name TEXT NOT NULL,labelPlate TEXT NOT NULL)')
+# conn.execute('CREATE TABLE user (id INTEGER PRIMARY KEY AUTOINCREMENT,username TEXT UNIQUE NOT NULL)')
+conn.execute("create table dataset_img (id INTEGER PRIMARY KEY AUTOINCREMENT,filename TEXT UNIQUE NOT NULL,label TEXT,status INT DEFAULT 0 NOT NULL,username TEXT  )")
+#cur.execute("select * from Plate where username='Binh'")
+#conn.execute('Drop table dataset_img')
+#conn.execute('CREATE TABLE Plate (id INTEGER PRIMARY KEY AUTOINCREMENT,username TEXT UNIQUE NOT NULL,image_name TEXT NOT NULL,labelPlate TEXT NOT NULL)')
 print ("Table created successfully")
 conn.close()
